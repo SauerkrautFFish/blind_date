c58ef5431d88d192e13d46e94b4251cc7a879591
SauerkrautFFish <knighthong0912@gmail.com>
2023-11-18 22:58:47 +0800

    完善自动化脚本

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ if [[ ! "$python_version" == *"Python 3.10.6"* ]]; then
         exit 1
     fi
     rm -rf Python-3.10.6.tgz
-    echo "下载python3.10成功"
+    echo "下载python3.10.6成功"
 
     echo "更新pip"
     # 更新pip到最新版本
@@ -62,7 +62,7 @@ fi
 
 echo "执行Django迁移命令"
 # 执行Django迁移命令
-python3.10 manage.py migrate
+python3 manage.py migrate
 
 if [ $? -ne 0 ]; then
     echo "迁移命令执行失败"
@@ -71,7 +71,7 @@ fi
 echo "迁移命令执行成功"
 echo "启动服务中..."
 # 启动Django服务
-nohup python3.10 manage.py runserver 0.0.0.0:$PORT &
+nohup python3 manage.py runserver 0.0.0.0:$PORT &
 echo "启动服务成功"
 
 exit 0
