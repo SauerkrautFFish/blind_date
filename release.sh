8ce3af30fb28df814613a2d530fdd6fde5f8f93e
SauerkrautFFish <knighthong0912@gmail.com>
2023-11-19 12:19:30 +0800

    完善自动化脚本

--- release.sh
+++ release.sh
@@ -2,14 +2,18 @@
 python_version=$(python3 --version 2>&1)
 # 检查当前机器是否安装了 Python
 if [[ ! "$python_version" == *"Python 3.10.6"* ]]; then
-    # 安装Python 3.10（使用yum包管理器）
+    # 安装环境
     # https://www.zhihu.com/question/456908213/answer/3248085449?utm_id=0
+    # https://www.jianshu.com/p/426596c4ff4c
+    # https://www.xjx100.cn/news/436747.html?action=onClick
+    # https://zhuanlan.zhihu.com/p/564255869
+
     echo "没有发现python, 开始执行相关下载"
 
     echo "下载zlib zlib-dev openssl-devel..."
     sudo yum install -y zlib zlib-dev openssl-devel sqlite-devel bzip2-devel libffi libffi-devel gcc gcc-c++
 
-    cd ~
+    cd ~ || exit 1
     echo "下载openssl1.1.1."
     wget https://www.openssl.org/source/openssl-1.1.1q.tar.gz
     echo "解压openssl1.1.1"
@@ -24,9 +28,26 @@ if [[ ! "$python_version" == *"Python 3.10.6"* ]]; then
     ldconfig
     rm -rf /usr/bin/openssl
     ln -s /usr/local/bin/openssl /usr/bin/openssl
-    echo "openssl安装完成"
-    cd ~
+    cd ~ || exit 1
     rm -rf openssl-1.1.1q.tar.gz
+    echo "openssl安装完成"
+
+    echo "下载sqlite"
+    wget https://www.sqlite.org/2022/sqlite-autoconf-3390300.tar.gz
+    echo "解压sqlite"
+    tar zxvf sqlite-autoconf-3390300.tar.gz
+    cd sqlite-autoconf-3390300/ || exit 1
+    ./configure
+    echo "编译sqlite"
+    make
+    echo "安装sqlite"
+    make install
+    rm -rf /usr/bin/sqlite3
+    ln -s /usr/local/bin/sqlite3 /usr/bin/sqlite3
+    cd ~ || exit 1
+    rm -rf sqlite-autoconf-3390300.tar.gz
+    echo "下载sqlite成功"
+
     echo "下载python3.10.6"
     wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tgz
     echo "解压python3.10.6"
